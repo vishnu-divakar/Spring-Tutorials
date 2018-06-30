34247bdea826f42253d030c9ea0bd29c6f0211dc
vishnu divakar <vishnu_divakar@hotmail.com>
2018-06-30 18:35:53 +0530

    Initial Commit

---
+++ src/main/java/com/spring/intro/Spring5Intro/Spring5IntroApplication.java
@@ -0,0 +1,12 @@
+package com.spring.intro.Spring5Intro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5IntroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5IntroApplication.class, args);
+	}
+}
---
+++ src/test/java/com/spring/intro/Spring5Intro/Spring5IntroApplicationTests.java
@@ -0,0 +1,16 @@
+package com.spring.intro.Spring5Intro;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Spring5IntroApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
